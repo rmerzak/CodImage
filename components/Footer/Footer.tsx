4297706f2e2cebf367c50cf3e4385ac964a38a43
rmerzak <rmerzak@student.1337.ma>
2024-01-02 17:24:04 +0100

    remove lottofie

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { Heart } from 'lucide-react'
-import Lottie from 'lottie-react'
 import animation from '@/public/animation.json'
 function Footer() {
     const defaultOptions = {
@@ -16,12 +15,12 @@ function Footer() {
             <Link className='text-sm text-gray-400 hover:text-gray-500' href='https://www.rmerzak.me'>rmerzak.me</Link>
             <Link href='' className='flex items-center text-sm  font-medium hover:text-slate-200 ease-in-out'>
                 <div className='relative flex items-center justify-between'>
-                <div className='flex items-center justify-end w-full h-full'>Made By</div>
-                {/* <Heart className='w-4 h-4 text-red-500' /> */}
-                    <Lottie
+                <div className='flex items-center w-full h-full'>Made By</div>
+                <Heart className=' w-4 h-4 text-red-500' />
+                    {/* <Lottie
                         animationData={animation}
                         className='flex float-end w-[100px] h-[100px] '
-                    />
+                    /> */}
                 </div>
             </Link>
             <Link href='' className='flex items-center text-sm font-medium hover:text-slate-200 ease-in-out'>
