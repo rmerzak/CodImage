eda031a391c5ba87568043ba9e3ccf120299b5b3
rmerzak <rmerzak@student.1337.ma>
2024-01-03 19:01:29 +0100

    update footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,15 +1,18 @@
 'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { Heart } from 'lucide-react'
 import animation from '@/public/animation.json'
+import Popup from '../CvUp/Popup'
+import OutsideClickHandler from 'react-outside-click-handler'
 function Footer() {
     const defaultOptions = {
         loop: true,
         autoplay: true,
         animationData: animation,
     };
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className='flex items-center gap-10 py-16'>
             <Link className='text-sm text-gray-400 hover:text-gray-500' href='https://www.rmerzak.me'>rmerzak.me</Link>
@@ -17,15 +20,21 @@ function Footer() {
                 <div className='relative flex items-center justify-between'>
                 <div className='flex items-center w-full h-full'>Made By</div>
                 <Heart className=' w-4 h-4 text-red-500' />
-                    {/* <Lottie
-                        animationData={animation}
-                        className='flex float-end w-[100px] h-[100px] '
-                    /> */}
                 </div>
             </Link>
-            <Link href='' className='flex items-center text-sm font-medium hover:text-slate-200 ease-in-out'>
+            <Link href='https://github.com/rmerzak/CodImage/tree/main' className='flex items-center text-sm font-medium hover:text-slate-200 ease-in-out'>
                 Source Code Here &nbsp;&#129120;
             </Link>
+            <div className='flex'>
+                <Link href='https://www.linkedin.com/in/rmerzak/'>
+                <Image src='/linkdeen.svg' alt='Vercel Logo' width={30} height={30} /></Link>
+                <Link href='https://github.com/rmerzak'>
+                <Image src='/github.svg' alt='Vercel Logo' width={30} height={30} /></Link>
+                <button onClick={() => setIsOpen(true)}>
+                <Image src='/portfolio.svg' alt='Vercel Logo' width={30} height={30} /></button>
+            </div>
+             {isOpen && 
+                <Popup setIsOpen={setIsOpen} />}
         </div>
     )
 }
